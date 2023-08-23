2762e21ca5ed9e02f3db2ff05325eeca8fb8b26a
NGanch <86801593+NGanch@users.noreply.github.com>
2023-08-23 17:51:18 +0100

    comit

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
+// const dotenv = require('dotenv');
+require('dotenv').config();
 const contactsRouter = require('./routes/api/contacts')
 
+// dotenv.config()
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
+// викликаємо cors
 app.use(cors())
 app.use(express.json())
 
@@ -19,7 +22,8 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  const {status = 500, message = "Server error"} = err;
+  res.status(status).json({ message, });
 })
 
-module.exports = app
+module.exports = app;
