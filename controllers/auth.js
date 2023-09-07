c62314ba901ede2617ef691e9064d0471e05e8c0
NGanch <86801593+NGanch@users.noreply.github.com>
2023-09-07 18:21:14 +0100

    comit

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,9 +68,7 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.json(204, "No Content")({
-
-  });
+  res.status(204).send();
 };
 
 module.exports = {
